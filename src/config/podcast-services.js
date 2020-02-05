f32bf68568505422c3f320329bc44d5b2a7c9247
Jamie <jamie@gaprogman.com>
2020-02-05 15:33:07 +0000

    Removed urls for podcast services that we don't have yet

--- src/config/podcast-services.js
+++ src/config/podcast-services.js
@@ -1,23 +1,24 @@
 export default [
   {
     name: 'Apple Podcasts',
-    url: 'https://podcasts.apple.com/podcast/the-net-core-podcast/id1433496275'
+
+    url: '' //https://podcasts.apple.com/podcast/the-net-core-podcast/id1433496275
   },
   {
     name: 'Google Podcasts',
-    url: 'https://www.google.com/podcasts?feed=aHR0cHM6Ly90aGVkb3RuZXRjb3JlcG9kY2FzdC5saWJzeW4uY29tL3Jzcw%3D%3D'
+    url: '' //https://www.google.com/podcasts?feed=aHR0cHM6Ly90aGVkb3RuZXRjb3JlcG9kY2FzdC5saWJzeW4uY29tL3Jzcw%3D%3D
   },
   {
     name: 'Stitcher',
-    url: 'https://www.stitcher.com/podcast/the-net-core-podcast'
+    url: '' //https://www.stitcher.com/podcast/the-net-core-podcast
   },
   {
     name: 'PocketCasts',
-    url: 'https://pca.st/yP3j'
+    url: '' //https://pca.st/yP3j
   },
   {
     name: 'Spotify',
-    url: 'https://open.spotify.com/show/6FIM18ZIqQvuX7d1TA9rbZ?si=V82TfPFLTHaBZSZAbH9_rQ'
+    url: '' //https://open.spotify.com/show/6FIM18ZIqQvuX7d1TA9rbZ?si=V82TfPFLTHaBZSZAbH9_rQ
   },
   {
     name: 'RSS',
