e4d5f0f85331da32a2eeddd715b21a659d7f4388
Jamie <jamie@gaprogman.com>
2020-02-05 15:57:50 +0000

    Hosts component can now link to a host url

--- src/components/Host.js
+++ src/components/Host.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export default ({ name, image, altText, description }) => (
+export default ({ name, image, altText, description, url }) => (
   <div className="col-4">
-    <span className="image fit">
-      <img src={image} alt={altText} />
-    </span>
-    <h3>{name}</h3>
-    <p>{description}</p>
+      <span className="image fit">
+        <img src={image} alt={altText} />
+      </span>
+      <a href={url.length > 0 ? url : "#"} target="_blank" rel="noopener noreferrer">
+        <h3>{name}</h3>
+      </a>
+      <p>{description}</p>
   </div>
 )
