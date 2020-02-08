7c89406570ec636f58d5da716794bc5e5ba0d262
Jamie Taylor <1607732+GaProgMan@users.noreply.github.com>
2020-02-08 20:11:57 +0000

    Added a bunch of stuff - mostly linting suggestions (#2)
    * Fixed some issues with linting on subscribe page
    
    * Rmeoved unused variable and commented out episode objects
    
    * index component now uses the arrow/function syntax
    
    * Removed unused include on index component
    
    * Hosts component can now link to a host url
    
    * Small tweaks for positioning of email subscription stuff on mailing list component
    
    * Added twitter url to footer. Also added target blank and rel values for security

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => (
     <section id="footer">
         <ul className="icons">
-            <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+            <li><a href="https://twitter.com/tabsnspacesHQ" className="icon alt fa-twitter" target="_blank" rel="noopener noreferrer"><span className="label">Twitter</span></a></li>
             <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
             <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
             <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
@@ -11,8 +11,8 @@ export default () => (
         </ul>
         <ul className="copyright">
             <li>tabs AND spaces &copy; {new Date().getFullYear()} </li>
-            <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-            <li>Show intro music: <a href="https://www.youtube.com/watch?v=n8X9_MgEdCg">Unity by The FatRat</a></li>
+            <li>Design: <a href="http://html5up.net" target="_blank" rel="noopener noreferrer">HTML5 UP</a></li>
+            <li>Show intro music: <a href="https://www.youtube.com/watch?v=n8X9_MgEdCg" target="_blank" rel="noopener noreferrer">Unity by The FatRat</a></li>
         </ul>
     </section>
 )
--- src/config/host-details.js
+++ src/config/host-details.js
@@ -7,18 +7,21 @@ export default [
     name: 'Zac Braddy',
     image: zacHeadShot,
     altText: 'Zac Braddy',
-    description: 'Zacker the Hacker is an Aussie living in the UK'
+    description: 'Zacker the Hacker is an Aussie living in the UK',
+    url : ''
   },
   {
     name: 'James Studdart',
     image: jamesHeadShot,
     altText: 'James Studdart',
-    description: 'James is a developer contractor from somewhere near Wales'
+    description: 'James is a developer contractor from somewhere near Wales',
+    url: ''
   },
   {
     name: 'Jamie Taylor',
     image: jamieHeadShot,
     altText: 'Jamie Taylor',
-    description: 'Jamie is a weirdo. Originally from Hull, he now lives in Leeds'
+    description: 'Jamie is a weirdo. Originally from Hull, he now lives in Leeds',
+    url: ''
   }
 ]
