dfc40df88f1b5ef70d7be11eef5cb53d19704418
Zac Braddy <zacharybraddy@gmail.com>
2020-02-13 09:56:28 +0000

    Added nav bar and fixed header logo on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import colourLogo from '../assets/images/colour-logo.svg'
-
+import headerLogo from '../assets/images/header-logo.svg'
 
 export default () => (
-  <section id="header">
-    <div className="inner">
-      <img
-        src={colourLogo}
-        className="logo"
-        alt="Tabs and spaces podcast. The truth is, you're always wrong"
-      />
-    </div>
-  </section>
+  <>
+    <nav className="menu-nav">
+      <a href="#header" className="menu-nav__link">
+        <img src={headerLogo} alt="Tabs and Spaces podcast logo" />
+      </a>
+      <ul className="menu-nav__inner">
+        <li className="menu-nav__item">
+          <a href="#show-description" className="menu-nav__link">
+            About the Show
+          </a>
+        </li>
+        <li className="menu-nav__item">
+          <a href="#latest-episodes" className="menu-nav__link">
+            Latest Episodes
+          </a>
+        </li>
+        <li className="menu-nav__item">
+          <a href="#hosts" className="menu-nav__link">
+            About Us
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <section id="header">
+      <div className="inner">
+        <img
+          src={colourLogo}
+          className="logo"
+          alt="Tabs and spaces podcast. The truth is, you're always wrong"
+        />
+      </div>
+    </section>
+  </>
 )
