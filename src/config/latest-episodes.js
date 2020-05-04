706999b8e6f516bf96e63e9c72a23fc7bbaa187b
jamie <seppydude@gmail.com>
2020-05-04 14:47:43 +0100

    Added episode 5, removed episode 2 from UI

--- src/config/latest-episodes.js
+++ src/config/latest-episodes.js
@@ -1,8 +1,6 @@
-//import ep1 from '../assets/images/ep001.jpg'
-import ep2 from '../assets/images/ep002.jpg'
 import ep3 from '../assets/images/ep003.jpg'
 import ep4 from '../assets/images/ep004.jpg'
-//import ep5 from '../assets/images/ep005.jpg'
+import ep5 from '../assets/images/ep005.jpg'
 
 // We should totally read these from config, when in production
 export default [
@@ -12,32 +10,26 @@ export default [
     image: ep1,
     altText: 'Album artwork for episode 1',
     episodeId: '13022324',
-  }*/ {
-    name: 'Toxic Workplaces',
-    subtitle: 'Should I stay or should I go?',
-    image: ep2,
-    altText: 'Album artwork for episode 2',
-    episodeId: '13202597',
-  },
+  }*/ 
   {
     name: 'Dealing with Burnout',
     subtitle: 'Gotta go fast! But gotta stay alive too!',
     image: ep3,
-    altText: 'Album atrwork for episode 3',
+    altText: 'Episode 3 covers the topic of burnout',
     episodeId: '13238294',
   },
   {
     name: 'Dealing with Imposter Syndrome',
     subtitle: `A topic we're barely qualified to cover`,
     image: ep4,
-    altText: 'Album atrwork for episode 4',
+    altText: 'Episode 4 introduces the idea of Imposter Syndrome',
     episodeId: '13594871',
   },
-  /*{
+  {
     name: 'To buy or to build',
     subtitle: `A quote by Bill Shakespeare`,
     image: ep5,
-    altText: 'Album artwork for episode 5',
+    altText: 'Episode 5 is related to whether you should buy or build you tools',
     episodeId: '14069129',
-  }*/
+  }
 ]
