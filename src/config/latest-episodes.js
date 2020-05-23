1c178f328d58408d93cba2a2921f3bf5d7b77901
Zac Braddy <zacharybraddy@gmail.com>
2020-05-23 10:54:36 +0100

    Added commented out episode 6 ready for release in a bit over a week

--- src/config/latest-episodes.js
+++ src/config/latest-episodes.js
@@ -1,16 +1,10 @@
 import ep3 from '../assets/images/ep003.jpg'
 import ep4 from '../assets/images/ep004.jpg'
 import ep5 from '../assets/images/ep005.jpg'
+//import ep6 from '../assets/images/ep006.jpg'
 
 // We should totally read these from config, when in production
 export default [
-  /*{
-    name: 'Should I learn ALL the latest tech?',
-    subtitle: `Because for real....there's so much of it.`,
-    image: ep1,
-    altText: 'Album artwork for episode 1',
-    episodeId: '13022324',
-  }*/ 
   {
     name: 'Dealing with Burnout',
     subtitle: 'Gotta go fast! But gotta stay alive too!',
@@ -29,7 +23,15 @@ export default [
     name: 'To buy or to build',
     subtitle: `A quote by Bill Shakespeare`,
     image: ep5,
-    altText: 'Episode 5 is related to whether you should buy or build you tools',
+    altText:
+      'Episode 5 is related to whether you should buy or build you tools',
     episodeId: '14069129',
-  }
+  },
+  /*{
+    name: 'Learning...how do?',
+    subtitle: `Sometimes it feels like barely at all`,
+    image: ep6,
+    altText: 'Episode 6 is all about learning and how we go about it',
+    episodeId: '14462672',
+  },*/
 ]
