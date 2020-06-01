f247bda7cc2136fd0395c0ebe9e7ce051733e010
Zac Braddy <zacharybraddy@gmail.com>
2020-06-01 08:40:24 +0100

    Made episode 6 live

--- src/config/latest-episodes.js
+++ src/config/latest-episodes.js
@@ -1,17 +1,17 @@
-import ep3 from '../assets/images/ep003.jpg'
+//import ep3 from '../assets/images/ep003.jpg'
 import ep4 from '../assets/images/ep004.jpg'
 import ep5 from '../assets/images/ep005.jpg'
-//import ep6 from '../assets/images/ep006.jpg'
+import ep6 from '../assets/images/ep006.jpg'
 
 // We should totally read these from config, when in production
 export default [
-  {
+  /*{
     name: 'Dealing with Burnout',
     subtitle: 'Gotta go fast! But gotta stay alive too!',
     image: ep3,
     altText: 'Episode 3 covers the topic of burnout',
     episodeId: '13238294',
-  },
+  },*/
   {
     name: 'Dealing with Imposter Syndrome',
     subtitle: `A topic we're barely qualified to cover`,
@@ -27,11 +27,11 @@ export default [
       'Episode 5 is related to whether you should buy or build you tools',
     episodeId: '14069129',
   },
-  /*{
+  {
     name: 'Learning...how do?',
     subtitle: `Sometimes it feels like barely at all`,
     image: ep6,
     altText: 'Episode 6 is all about learning and how we go about it',
     episodeId: '14462672',
-  },*/
+  },
 ]
