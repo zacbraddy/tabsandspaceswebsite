0a60cec21d801057ac6fef39e8d801a6af137686
Zac Braddy <zacharybraddy@gmail.com>
2020-06-28 20:47:10 +0100

    Adding ep007 ready for release in July

--- src/config/latest-episodes.js
+++ src/config/latest-episodes.js
@@ -1,6 +1,7 @@
 import ep4 from '../assets/images/ep004.jpg'
 import ep5 from '../assets/images/ep005.jpg'
 import ep6 from '../assets/images/ep006.jpg'
+/*import ep7 from '../assets/images/ep007.jpg'*/
 
 // We should totally read these from config, when in production
 export default [
@@ -26,4 +27,11 @@ export default [
     altText: 'Episode 6 is all about learning and how we go about it',
     episodeId: '14462672',
   },
+  /*{
+    name: 'Bringing your best interview game',
+    subtitle: `First impressions last...trust us`,
+    image: ep7,
+    altText: 'Episode 7 is about out to do your best in job interviews',
+    episodeId: '14996825',
+  }*/
 ]
