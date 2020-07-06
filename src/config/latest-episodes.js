1b8708dfff170bf57695a5812495065c687d2f0d
Zac Braddy <zacharybraddy@gmail.com>
2020-07-06 09:30:19 +0100

    Made episode 7 live and removed episode 4

--- src/config/latest-episodes.js
+++ src/config/latest-episodes.js
@@ -1,17 +1,9 @@
-import ep4 from '../assets/images/ep004.jpg'
 import ep5 from '../assets/images/ep005.jpg'
 import ep6 from '../assets/images/ep006.jpg'
-/*import ep7 from '../assets/images/ep007.jpg'*/
+import ep7 from '../assets/images/ep007.jpg'
 
 // We should totally read these from config, when in production
 export default [
-  {
-    name: 'Dealing with Imposter Syndrome',
-    subtitle: `A topic we're barely qualified to cover`,
-    image: ep4,
-    altText: 'Episode 4 introduces the idea of Imposter Syndrome',
-    episodeId: '13594871',
-  },
   {
     name: 'To buy or to build',
     subtitle: `A quote by Bill Shakespeare`,
@@ -27,11 +19,11 @@ export default [
     altText: 'Episode 6 is all about learning and how we go about it',
     episodeId: '14462672',
   },
-  /*{
+  {
     name: 'Bringing your best interview game',
     subtitle: `First impressions last...trust us`,
     image: ep7,
     altText: 'Episode 7 is about out to do your best in job interviews',
     episodeId: '14996825',
-  }*/
+  },
 ]
