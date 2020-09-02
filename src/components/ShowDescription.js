1b492cad75f661938d20281f9101400504056444
Zac Braddy <zacharybraddy@gmail.com>
2020-09-02 23:10:56 +0100

    Added trailer to show description

--- src/components/ShowDescription.js
+++ src/components/ShowDescription.js
@@ -14,6 +14,23 @@ export default ({ siteDescription, siteTitle }) => (
           software developers throughout their career and we promise that we
           won't always agree!
         </p>
+        <div className="trailer-container">
+          <h3>Wanna hear a little more about it?</h3>
+          <div className="trailer-player">
+            <iframe
+              style={{ border: 'none' }}
+              src="//html5-player.libsyn.com/embed/episode/id/15861791/height/90/theme/custom/thumbnail/yes/direction/backward/render-playlist/no/custom-color/222222/menu/no/"
+              height="90"
+              width="100%"
+              scrolling="no"
+              allowFullScreen
+              webkitallowfullscreen="true"
+              mozallowfullscreen="true"
+              oallowfullscreen="true"
+              msallowfullscreen="true"
+            ></iframe>
+          </div>
+        </div>
       </div>
     </div>
   </section>
