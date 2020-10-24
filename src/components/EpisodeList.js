943e2f2b6e2b932706eda2f614a70159db39441b
Zac Braddy <zacharybraddy@gmail.com>
2020-10-24 21:42:21 +0100

    Added contentful for managing episodes

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import Episode from './Episode'
-import latest from '../config/latest-episodes'
 import services from '../config/podcast-services'
 
 export default () => {
+  const extractData = (ep) => ({
+    name: ep.node.episodeName.episodeName,
+    subtitle: ep.node.episodeSubtitle.episodeSubtitle,
+    image: ep.node.episodeArt.file.url,
+    altText: ep.node.episodeArtAltText.episodeArtAltText,
+    episodeId: ep.node.libsynId,
+  })
+
+  const query = useStaticQuery(graphql`
+    query latestEpisodes {
+      allContentfulEpisode(
+        limit: 3
+        sort: { fields: [seasonNumber, episodeNumber], order: [DESC, DESC] }
+      ) {
+        edges {
+          node {
+            episodeName {
+              episodeName
+            }
+            episodeSubtitle {
+              episodeSubtitle
+            }
+            episodeArt {
+              file {
+                url
+              }
+            }
+            libsynId
+            episodeArtAltText {
+              episodeArtAltText
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const data = query.allContentfulEpisode.edges.map(extractData)
+
   return (
     <section id="latest-episodes" className="main style3 special">
       <div className="grid-wrapper">
@@ -13,9 +52,10 @@ export default () => {
           </header>
           <p>Check out our latest episodes</p>
         </div>
-        {latest.map((ep, idx) => (
-          <Episode key={idx} {...ep} />
-        ))}
+        {data.map((ep, idx) => {
+          console.log(ep)
+          return <Episode key={idx} {...ep} />
+        })}
         <div className="col-12" style={{ marginTop: '3rem' }}>
           But also don't forget to check out the rest and subscribe to us on
           your favourite podcast destination
