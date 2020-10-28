49d27feb5d91c7a71d1b373a816f3c587b47675b
Zac Braddy <zacharybraddy@gmail.com>
2020-10-28 23:51:51 +0000

    Removed service worker and added mobile menu

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,25 +7,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-head',
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/play-logo.svg', // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-158809546-1',
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
     {
       resolve: `gatsby-source-contentful`,
       options: {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,94 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useState } from 'react'
+import { Link, navigate } from 'gatsby'
+import { slide as Menu } from 'react-burger-menu'
 import colourLogo from '../assets/images/colour-logo.svg'
 import headerLogo from '../assets/images/header-logo.svg'
 
-export default () => (
-  <>
-    <nav className="menu-nav">
-      <a href="/#header" className="menu-nav__link">
-        <img src={headerLogo} alt="Tabs and Spaces podcast logo" />
-      </a>
-      <ul className="menu-nav__inner">
-        <li className="menu-nav__item">
-          <Link to="/#show-description" className="menu-nav__link">
-            About the show
+export default () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const mobileNavigate = (to) => () => {
+    navigate(to)
+    setMenuOpen(false)
+  }
+
+  return (
+    <>
+      <nav className="menu-nav">
+        <div className="menu-nav__mobile">
+          <Menu
+            isOpen={menuOpen}
+            onStateChange={({ isOpen }) => setMenuOpen(isOpen)}
+          >
+            <a className="menu-nav__link" onClick={mobileNavigate('/#header')}>
+              <img src={headerLogo} alt="Tabs and Spaces podcast logo" />
+            </a>
+
+            <ul className="menu-nav__inner">
+              <li className="menu-nav__item">
+                <a
+                  onClick={mobileNavigate('/#latest-episodes')}
+                  className="menu-nav__link"
+                >
+                  Just the latest episodes
+                </a>
+              </li>
+              <li className="menu-nav__item">
+                <a
+                  onClick={mobileNavigate('/episodes')}
+                  className="menu-nav__link"
+                >
+                  All the episodes
+                </a>
+              </li>
+              <li className="menu-nav__item">
+                <a
+                  onClick={mobileNavigate('/#hosts')}
+                  className="menu-nav__link"
+                >
+                  About us
+                </a>
+              </li>
+            </ul>
+          </Menu>
+        </div>
+        <div className="menu-nav__desktop">
+          <Link to="/#header" className="menu-nav__link">
+            <img src={headerLogo} alt="Tabs and Spaces podcast logo" />
           </Link>
-        </li>
-        <li className="menu-nav__item">
-          <Link to="/#latest-episodes" className="menu-nav__link">
-            Just the latest episodes
-          </Link>
-        </li>
-        <li className="menu-nav__item">
-          <Link to="/episodes" className="menu-nav__link">
-            All the episodes
-          </Link>
-        </li>
-        <li className="menu-nav__item">
-          <Link to="/#hosts" className="menu-nav__link">
-            About us
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <section id="header">
-      <div className="inner">
-        <img
-          src={colourLogo}
-          className="logo"
-          alt="Tabs and spaces podcast. The truth is, you're always wrong"
-        />
-      </div>
-    </section>
-  </>
-)
+          <ul className="menu-nav__inner">
+            <li className="menu-nav__item">
+              <Link to="/#show-description" className="menu-nav__link">
+                About the show
+              </Link>
+            </li>
+            <li className="menu-nav__item">
+              <Link to="/#latest-episodes" className="menu-nav__link">
+                Just the latest episodes
+              </Link>
+            </li>
+            <li className="menu-nav__item">
+              <Link to="/episodes" className="menu-nav__link">
+                All the episodes
+              </Link>
+            </li>
+            <li className="menu-nav__item">
+              <Link to="/#hosts" className="menu-nav__link">
+                About us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <section id="header">
+        <div className="inner">
+          <img
+            src={colourLogo}
+            className="logo"
+            alt="Tabs and spaces podcast. The truth is, you're always wrong"
+          />
+        </div>
+      </section>
+    </>
+  )
+}
